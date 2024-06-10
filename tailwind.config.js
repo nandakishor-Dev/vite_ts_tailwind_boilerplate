f5a02fe2caf0c26ee87e064f7e2d9c97e6c6e9b5
ChaavieNandakishor <chaavienandakishor@gmail.com>
2024-06-10 17:41:30 +0530

    added router

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F1F1F1',
+      }
+    }
   },
   plugins: [],
 }
